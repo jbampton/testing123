b7f46a3f10fbf096f3abb030e912d3ed89dc3185
John Bampton <jbampton@gmail.com>
2023-07-17 23:25:17 +1000

    Add more stats and clean up Python code

--- run.py
+++ run.py
@@ -4,22 +4,27 @@ import urllib.request
 from datetime import datetime
 
 
-p = re.compile(r"(<!-- lichess -->)(.*?)(<!-- lichess -->)", re.MULTILINE | re.DOTALL)
-with urllib.request.urlopen('https://lichess.org/api/user/SexyMate/perf/blitz') as url:
-    data = json.load(url)
-    results = ''
-    for row in data['stat']['bestWins']['results']:
-        title = row['opId']['title']
-        if title == None:
-            title = ''
-        else:
-            title += ' '
-        name = row['opId']['name']
-        rating = row['opRating']
-        date = row['at']
-        results += f'- {title}{name} ({rating}) {date}\n'
-    with open("README.md", 'r') as my_file:
-        readme = my_file.read()
-        readme = re.sub(p, r"\1\n"+results+r"\3", readme)
-    with open("README.md", 'w') as my_file:
-        my_file.write(readme)
+pattern = re.compile(r"(<!-- lichess -->)(.*?)(<!-- lichess -->)", re.MULTILINE | re.DOTALL)
+perfTypes = ['bullet', 'blitz', 'rapid'] # 'classical', 'correspondence', 'chess960', 'crazyhouse']
+sections = ''
+for perf in perfTypes:
+    with urllib.request.urlopen(f'https://lichess.org/api/user/SexyMate/perf/{perf}') as url:
+        data = json.load(url)
+        results = f'## Best {perf} wins\n\n'
+        for row in data['stat']['bestWins']['results']:
+            title = row['opId']['title']
+            if title == None:
+                title = ''
+            else:
+                title += ' '
+            name = row['opId']['name']
+            rating = row['opRating']
+            date = row['at']
+            results += f'- {title}{name} ({rating}) {date}\n'
+        results += '\n'
+        sections += results
+with open("README.md", 'r') as my_file:
+    readme = my_file.read()
+    readme = re.sub(pattern, r"\1\n"+sections+r"\3", readme)
+with open("README.md", 'w') as my_file:
+    my_file.write(readme)
